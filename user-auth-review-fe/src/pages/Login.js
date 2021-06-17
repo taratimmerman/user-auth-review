67c475bcc63bbb2e53aebbc17f34aa9ae01bb201
taratimmerman <timmermantara@gmail.com>
2021-06-17 17:38:36 -0600

    Implemented form validation

--- user-auth-review-fe/src/pages/Login.js
+++ user-auth-review-fe/src/pages/Login.js
@@ -1,8 +1,11 @@
-import { React, useState } from 'react';
+/* eslint-disable no-unused-vars */
+import { React, useState, useEffect } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import { formSchema } from '../validation/formSchema';
 
 const initialFormValues = {
     username: '',
@@ -11,11 +14,28 @@ const initialFormValues = {
 
 const Login = () => {
     const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState('');
+    const [disabled, setDisabled] = useState(true);
     const history = useHistory();
 
-    const handleChange = event => {
+    const handleChange = async event => {
         const { name, value } = event.target;
         setFormValues({...formValues, [name]: value});
+        Yup
+            .reach(formSchema, name)
+            .validate(value)
+            .then(() => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: '',
+                });
+            })
+            .catch((err) => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: err.errors[0]
+                });
+            });
     };
 
     const login = (event) => {
@@ -28,9 +48,15 @@ const Login = () => {
                 history.push('/dashboard');
             })
             .catch((err) => {
-                console.log(err);
+                alert(err);
             });
     };
+
+    useEffect(() => {
+        formSchema.isValid(formValues).then((valid) => {
+            setDisabled(!valid);
+        });
+    }, [formValues]);
 
     return (
         <section className='wrapper'>
@@ -43,7 +69,10 @@ const Login = () => {
                     variant='outlined'
                     value={formValues.username}
                     onChange={handleChange}
+                    error={formErrors?.username}
+                    helperText={(formErrors?.username)}
                     maxLength='255'
+                    required
                 />
                 <TextField
                     type='password'
@@ -52,17 +81,21 @@ const Login = () => {
                     variant='outlined'
                     value={formValues.password}
                     onChange={handleChange}
+                    error={formErrors?.password}
+                    helperText={(formErrors?.password)}
                     maxLength='255'
+                    required
                 />
                 <Button
                     variant='contained'
                     color='primary'
                     type='submit'
                     onClick={login}
+                    disabled={disabled}
                 >Log in</Button>
             </form>
             <div className='switch'>
-                <span>Need an account? <Button color='primary' component={Link} to={'/registration'}>Register here</Button></span>
+                <span>Need an account? <Button color='primary' component={Link} to={'/registration'} >Register here</Button></span>
             </div>
         </section>
     );
